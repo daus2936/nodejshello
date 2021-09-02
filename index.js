66d69225d5bf9f629d04c2f5b7199d9d1072b84e
daus2936 <dausadinegoro@gmail.com>
2021-09-02 07:23:52 +0000

    tes2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello guyssssssssssssssssssss!');
+  res.send('Hello ssssssssssssssssssss!');
 });
 
 var server = app.listen(3000, function () {
